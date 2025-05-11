28bf04e6ba1de96c124800a01ae33012a0b99080
TaiTot2001 <nguyentrongtaihong@gmail.com>
2025-05-11 11:39:10 +0700

    đẩy lên LinearEquationResolver

---
+++ src/LinearEquationResolver.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LinearEquationResolver {
+    public static void main(String[] args) {
+        System.out.println("Giải phương trình bậc nhất a*x + b = c");
+        Scanner sc = new Scanner(System.in);
+        double a, b, c;
+        System.out.println("a : ");
+        a = sc.nextDouble();
+        System.out.println("b : ");
+        b = sc.nextDouble();
+        System.out.println("c : ");
+        c = sc.nextDouble();
+        if (a != 0) {
+            double answer = (c - b) / a;
+            System.out.printf("x = %.2f!", answer);
+        }
+        else {
+            if (b==c){
+                System.out.print("Phương trình có vô số nghiệm !");
+            }
+            else {
+                System.out.print("Phương trình vô nghiệm !");
+            }
+        }
+    }
+}
