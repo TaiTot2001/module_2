164262e27cff1cfe45cd6411c68a566d5a3ca1a0
TaiTot2001 <nguyentrongtaihong@gmail.com>
2025-05-15 19:23:22 +0700

    đẩy lên SumOfNumbersInAColumn_lesson3

---
+++ lesson3_ArraysAndMethods/SumOfNumbersInAColumn.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SumOfNumbersInAColumn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hãy nhập vào kích thước để khởi tạo mảng 2 chiều: ");
+        System.out.print("Nhập số hàng: ");
+        int rows = sc.nextInt();
+        System.out.print("Nhập số cột: ");  // có dấu cách để đẹp
+        int cols = sc.nextInt();
+        int[][] arr = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("Nhập vào giá trị thứ "+j+" của hàng " +i+": " );
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Mảng 2 chiều vừa nhập là:");
+        for (int i = 0; i < rows; i++) {
+            System.out.print("                           ");
+            for (int j = 0; j < cols; j++) {
+                System.out.print(arr[i][j]+"\t");
+            }
+            System.out.println();
+        }
+        System.out.print("Bạn muốn tính tổng của cột thứ mấy? ");
+        int total_cols = sc.nextInt();
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i][total_cols-1];
+        }
+        System.out.println("Tổng các phần tử của cột "+total_cols+" là: "+total);
+    }
+}
