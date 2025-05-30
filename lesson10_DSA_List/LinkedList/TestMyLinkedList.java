73a2d5008d798159abb0563c5ef081ceee549d9a
TaiTot2001 <nguyentrongtaihong@gmail.com>
2025-05-30 13:09:38 +0700

    đẩy lên bài tập LinkedList_ lesson10

---
+++ lesson10_DSA_List/LinkedList/TestMyLinkedList.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(4,9);
+//        ll.add(4,9);
+
+        System.out.println(ll.get(4));
+        ll.printList();
+    }
+}
