a3224379e1888408c713ea57a628fab9bd2c5a85
TaiTot2001 <nguyentrongtaihong@gmail.com>
2025-06-13 19:17:35 +0700

    đẩy lên bài tập thực hành  ArrayExample_ lesson15

--- lesson15_Debug/ArrayExample.java
+++ lesson15_Debug/ArrayExample.java
@@ -3,16 +3,17 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ArrayExample {
-    public Integer[] createRandom(){
+    public Integer[] createRandom() {
         Random rd = new Random();
         Integer[] arr = new Integer[100];
         System.out.println("Danh sách phần tử của mảng: ");
-        for(int i = 0; i < 100; i++){
+        for (int i = 0; i < 100; i++) {
             arr[i] = rd.nextInt(100);
         }
         System.out.println(Arrays.toString(arr));
         return arr;
     }
+
     public static void main(String[] args) {
         ArrayExample arrayExample = new ArrayExample();
         Integer[] arr = arrayExample.createRandom();
@@ -20,7 +21,7 @@ public class ArrayExample {
         System.out.print("Vui lòng nhập vào chỉ số của một phần tử bất kỳ: ");
         int x = scanner.nextInt();
         try {
-            System.out.println("Giá trị của phần tử có chỉ số "+x+" là: "+ arr[x]);
+            System.out.println("Giá trị của phần tử có chỉ số " + x + " là: " + arr[x]);
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Chỉ số vượt quá giới hạn của mảng!");
         }
