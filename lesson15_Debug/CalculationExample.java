8099f6fdf24139a5f5dd117a01b7185eddba953a
TaiTot2001 <nguyentrongtaihong@gmail.com>
2025-06-13 19:43:25 +0700

    đẩy lên bài tập thực hành  CalculationExample_ lesson15

--- lesson15_Debug/CalculationExample.java
+++ lesson15_Debug/CalculationExample.java
@@ -11,18 +11,19 @@ public class CalculationExample {
         calc.calculation(numberOne, numberTwo);
 
     }
+
     public void calculation(int numberOne, int numberTwo) {
         try {
             int addition = numberOne + numberTwo;
             int subtract = numberOne - numberTwo;
             int multiply = numberOne * numberTwo;
             int divide = numberOne / numberTwo;
-            System.out.println("Kết quả của x + y = "+addition);
-            System.out.println("Kết quả của x - y = "+subtract);
-            System.out.println("Kết quả của x * y = "+multiply );
-            System.out.println("Kết quả của x / y = "+divide);
+            System.out.println("Kết quả của x + y = " + addition);
+            System.out.println("Kết quả của x - y = " + subtract);
+            System.out.println("Kết quả của x * y = " + multiply);
+            System.out.println("Kết quả của x / y = " + divide);
         } catch (ArithmeticException e) {
-            System.out.println("Giá trị của y là khác 0 !"+e.getMessage());
+            System.out.println("Giá trị của y là khác 0 !" + e.getMessage());
         }
     }
 }
