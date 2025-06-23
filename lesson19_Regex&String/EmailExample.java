b290d61935d0f20a585b08542fd89b8139fa8a23
TaiTot2001 <nguyentrongtaihong@gmail.com>
2025-06-23 19:30:39 +0700

    đẩy lên bài tập ThucHanh1_EmailExample_ lesson19

--- lesson19_Regex&String/EmailExample.java
+++ lesson19_Regex&String/EmailExample.java
@@ -4,27 +4,30 @@ import java.util.regex.Pattern;
 public class EmailExample {
     private static Pattern pattern;
     private static Matcher matcher;
-    private static final String Email_Regex ="^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+    private static final String Email_Regex = "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+
     public EmailExample() {
         pattern = Pattern.compile(Email_Regex);
     }
+
     public boolean validate(String regex) {
         matcher = pattern.matcher(regex);
         return matcher.matches();
     }
 
     private static EmailExample emailExample;
-    public static final String[] validEmails = new String[] {"1@g.com","a@i.o","2@j.1"};
-    public static final String[] invalidEmails =new String[]{".@.com","@j.1","j@%aa.com"};
+    public static final String[] validEmails = new String[]{"1@g.com", "a@i.o", "2@j.1"};
+    public static final String[] invalidEmails = new String[]{".@.com", "@j.1", "j@%aa.com"};
+
     public static void main(String[] args) {
         emailExample = new EmailExample();
         for (String email : validEmails) {
-            boolean isvalid =emailExample.validate(email);
-            System.out.println(email+": "+isvalid);
+            boolean isvalid = emailExample.validate(email);
+            System.out.println(email + ": " + isvalid);
         }
         for (String email : invalidEmails) {
-            boolean isvalid =emailExample.validate(email);
-            System.out.println(email+": "+isvalid);
+            boolean isvalid = emailExample.validate(email);
+            System.out.println(email + ": " + isvalid);
         }
     }
 
